db17696e8cc734452494be2c9b915fa4209f4b82
sezgin <sezgin.akgull@gmail.com>
2023-01-31 11:42:49 +0300

    Adding intro component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import About from "./components/about/About";
+import Intro from "./components/intro/Intro";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Intro />
+      <About />
+    </div>
+  );
 }
 
 export default App;

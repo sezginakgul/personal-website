41398d901119c4997b5c008768c572b7cdad9d60
sezgin <sezgin.akgull@gmail.com>
2023-03-02 09:22:16 +0300

    add new project

--- src/data.js
+++ src/data.js
@@ -3,37 +3,37 @@ import movie from "./img/movie.png";
 import contact from "./img/contact.png";
 import recipe from "./img/recipe.png";
 import tourplace from "./img/tourplace.png";
-import randomuser from "./img/randomuser.png";
 import weather from "./img/weather.png";
 import todo from "./img/todo.png";
 import calculator from "./img/calculator.png";
+import miniblog from "./img/miniblog.png";
 
 export const products = [
   {
     id: 1,
+    link: "https://s-mini-blog.vercel.app/",
+    img: miniblog,
+  },
+  {
+    id: 2,
     link: "https://s-blog-app.vercel.app/",
     img: blog,
   },
   {
-    id: 2,
+    id: 3,
     link: "https://s-contact-app.vercel.app/",
     img: contact,
   },
   {
-    id: 3,
+    id: 4,
     link: "https://movie-app-sezginakgul.vercel.app/",
     img: movie,
   },
 
   {
-    id: 4,
+    id: 5,
     link: "https://reactjs-recipe-appv1.vercel.app/",
     img: recipe,
-  },
-  {
-    id: 5,
-    link: "https://sezginakgul.github.io/random-user-appV2/",
-    img: randomuser,
   },
   {
     id: 6,
